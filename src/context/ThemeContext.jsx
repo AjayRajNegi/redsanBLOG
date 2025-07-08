22ab5aed125f7d2b80649dd537a4c6dc7dcf154d
AjayRajNegi <ajayrajnegi111@gmail.com>
2025-07-08 10:35:50 +0530

    Removed localStorage communication.

--- src/context/ThemeContext.jsx
+++ src/context/ThemeContext.jsx
@@ -4,16 +4,16 @@ import { createContext, useEffect, useState } from "react";
 
 export const ThemeContext = createContext();
 
-const getFromLocalStorage = () => {
-  if (typeof window !== "undefined") {
-    const value = localStorage.getItem("theme");
-    return value || "light";
-  }
-};
+// const getFromLocalStorage = () => {
+//   if (typeof window !== "undefined") {
+//     const value = localStorage.getItem("theme");
+//     return value || "light";
+//   }
+// };
 
 export const ThemeContextProvider = ({ children }) => {
   const [theme, setTheme] = useState(() => {
-    return getFromLocalStorage();
+    return "light";
   });
 
   const toggle = () => {
