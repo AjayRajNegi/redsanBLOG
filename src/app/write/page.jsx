a20e0c6eefd098da71450a99e75c937d3ccb5297
AjayRajNegi <ajayrajnegi111@gmail.com>
2025-07-08 10:43:17 +0530

    ReackQuill ke mkc!!

--- src/app/write/page.jsx
+++ src/app/write/page.jsx
@@ -1,5 +1,8 @@
 "use client";
 export const dynamic = "force-dynamic";
+const ReactQuill = dynamic(() => import("react-quill"), {
+  ssr: false,
+});
 
 import Image from "next/image";
 import styles from "./writePage.module.css";
