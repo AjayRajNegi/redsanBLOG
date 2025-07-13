786c6061625ccd672011945c97d8c8565e4a839b
AjayRajNegi <ajayrajnegi111@gmail.com>
2025-07-13 23:40:55 +0530

    Too much indecisive

--- src/app/layout.js
+++ src/app/layout.js
@@ -9,7 +9,7 @@ import AuthProvider from "@/providers/AuthProvider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "redsanBlog App",
+  title: "redsanBlog",
   description: "The best blog app!",
 };
 
